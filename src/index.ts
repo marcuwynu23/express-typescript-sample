8b0cbdd7ee42be1a7ae3b798df3d796b09d85014
Mark Wayne Menorca <marcuwynu23@gmail.com>
2026-05-20 16:05:51 +0800

    feat: integrate Swagger UI for API documentation and add OpenAPI specification

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import './config/config';
 import './tracer/tracer';
 import { trace } from '@opentelemetry/api';
 import express, { type Request, type Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { config } from './config/config';
 import { httpLogger } from './middlewares/httpLogger';
 import { metricsHandler, metricsMiddleware } from './middlewares/metrics';
@@ -22,6 +24,8 @@ app.get('/api/health', (_req: Request, res: Response) => {
   console.log('traceId:', span?.spanContext()?.traceId);
   res.json({ status: 'ok', timestamp: new Date().toISOString() });
 });
+const swaggerDocument = YAML.load('./src/swagger/openapi.yaml');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 if (require.main === module) {
   app.listen(config.port, config.host, () => {
